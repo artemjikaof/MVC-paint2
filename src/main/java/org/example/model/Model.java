2cb4bd0f5b0346f3974225f98260f01e2c3062c5
airybinov <jikafosters5@gmail.com>
2024-11-01 11:48:09 +0300

    что то сделал с ActionDraw

--- src/main/java/org/example/model/Model.java
+++ src/main/java/org/example/model/Model.java
@@ -2,6 +2,8 @@ package org.example.model;
 
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 
 // TODO: 25.10.2024 Сделать singleton класс
@@ -14,11 +16,20 @@ public class Model extends Observable {
 
     public void changeShape(Point2D x, Point2D y) {
         currentShape.setFrame(x, y);
-        this.setChanged();
-        this.notifyObservers();
+        update();
     }
 
     public void draw(Graphics2D g) {
         currentShape.draw(g);
+    }
+
+    public void update() {
+        this.setChanged();
+        this.notifyObservers();
+    }
+    private List<MyShape> shapeList = new ArrayList<>();
+
+    public void createCurrentShape(MyShape shape){
+        shapeList.add(shape);
     }
 }
