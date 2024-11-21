85310730049ed82466e6f5ceb9a574b04e9d1750
artemjikaof <jikafosters5@gmail.com>
2024-11-21 22:27:13 +0300

    Убрал ошибки getShapeType и isFill в классе ShapeCreationFactory. Добавил разные public в класс MenuState(как раз помогло для исправления ошибок в ShapeCreationFactory). пока всё

--- src/main/java/org/example/controller/factory/MenuState.java
+++ src/main/java/org/example/controller/factory/MenuState.java
@@ -12,11 +12,42 @@ public class MenuState {
     private boolean fill;
     private Color color;
     private ShapeType shapeType;
-    private ActionDraw action;
+    private ActionDraw actionDraw;
 
     public MenuState(){
         shapeType= ShapeType.RECTANGLE;
         color = Color.BLUE;
         fill = false;
+    }
+    public boolean isFill() {
+        return fill;
+    }
+
+    public void setFill(boolean fill) {
+        this.fill = fill;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public ShapeType getShapeType() {
+        return shapeType;
+    }
+
+    public void setShapeType(ShapeType shapeType) {
+        this.shapeType = shapeType;
+    }
+
+    public ActionDraw getActionDraw() {
+        return actionDraw;
+    }
+
+    public void setActionDraw(ActionDraw actionDraw) {
+        this.actionDraw = actionDraw;
     }
 }
